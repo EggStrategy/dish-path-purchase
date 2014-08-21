9332bb855b8ebbc512999b507d8fc37cd480e445
Brad Sawicki <git@bradsawicki.com>
2014-08-21 18:10:04 -0500

    Stage 5 BG / Circles / Paths

--- views/stage-5.php
+++ views/stage-5.php
@@ -1,17 +1,17 @@
   
 <section id="stage-5" class="stage stage-5 section">
 
+  <div class="stage-bg"></div>
+
   <div class="stage-left">
   
-    <div class="triggers fadeInUp animated">
-      <h2>Triggers</h2>
+    <div class="triggers">
       <ul>
         <li>Quality customer service</li>
         <li>Convenient and fast installation process</li>
         <li>Simple to use, reliable technology</li>
         <li>Clear and transparent billing</li>
       </ul>
-      <div class="arrow-triggers"></div>
     </div>
 
     <div class="barriers">
@@ -23,13 +23,16 @@
         <li>Poor customer service experiences</li>
       </ul>
     </div>
+    
+    <div class="circles"></div>
+    <div class="arrows"></div>
   
   </div>
   
   <div class="stage-right">
     
     <div class="stage-header">
-      <div class="stage-position">4 / 5</div>
+      <div class="stage-position">5 / 5</div>
       <h1 class="stage-title">Switch</h1>
       <p class="stage-question">Which TV provider will provide an improved experience?</p>
     </div>
@@ -44,6 +47,9 @@
       <div class="col thinking">
         <h3>Thinking</h3>
         <p>Decision criteria, the belief in a better service</p>
+      </div>
+      
+      <div class="urgency-note">
         <p>Outside life events, such as an impending move or baby, create urgency that results in a higher likelihood to quickly secure a new TV provider.</p>
       </div>
     
@@ -58,10 +64,10 @@
       <div class="mood">Excited</div>
     </div>
     
-    <div class="path-1" data-segment="indulgers"></div>
+<!--     <div class="path-1" data-segment="indulgers"></div> -->
     <div class="path-2" data-segment="enthusiasts"></div>
-    <div class="path-3" data-segment="flexibles"></div>
-    <div class="path-4" data-segment="traditionals"></div>
+<!--     <div class="path-3" data-segment="flexibles"></div> -->
+<!--     <div class="path-4" data-segment="traditionals"></div> -->
     
   </div>
   
