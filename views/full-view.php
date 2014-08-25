af776e70a7bb526aebb66da50ee873356e39266d
Brad Sawicki <git@bradsawicki.com>
2014-08-25 13:26:30 -0500

    Full View: Dish logo / Stage content update

--- views/full-view.php
+++ views/full-view.php
@@ -12,15 +12,39 @@
 
   <div class="full-view-right">
   
-    <div class="stage-label-5">5 &ndash; Switch</div>
-    <div class="stage-label-4">4 &ndash; Resolve</div>
-    <div class="stage-label-3">3 &ndash; Eveluation</div>
-    <div class="stage-label-2">2 &ndash; Exploration</div>
-    <div class="stage-label-1">1 &ndash; Preparation</div>
+    <div class="stage-label-5">
+      <div class="number">5 &ndash;</div>
+      <h3>Switch</h3>
+      <p>Which TV provider will provide an improved experience?</p>
+    </div>
+    <div class="stage-label-4">
+      <div class="number">4 &ndash;</div>
+      <h3>Resolve</h3>
+      <p>How can I fix my current situation and avoid switching TV providers?</p>
+    </div>
+    <div class="stage-label-3">
+      <div class="number">3 &ndash;</div>
+      <h3>Eveluation</h3>
+      <p>Which TV provider is best for me?</p>
+    </div>
+    <div class="stage-label-2">
+      <div class="number">2 &ndash;</div>
+      <h3>Exploration</h3>
+      <p>What are my options for a new TV provider?</p>
+    </div>
+    <div class="stage-label-1">
+      <div class="number">1 &ndash;</div>
+      <h3>Preparation</h3>
+      <p>What are my TV provider needs and priorities?</p>
+    </div>
   
+  </div>
+  
+  <div class="dish-logo">
+    <div class="logo"></div>
+    <h4>Consumer Path to Purchase Journey</h4>
   </div>
 
   <div class="full-fountain"></div>
 
-    
 </section>
